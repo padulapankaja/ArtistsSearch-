3bae8348d25366fe3e8373b205042f18e39c442c
lucidex <padulaguruge@gmail.com>
2020-02-26 14:16:03 +0530

    add about me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -8,10 +8,27 @@
 
 <script>
 import Navbar from "./Navbar.vue";
-
+import axios from "axios"
+import VueRouter from 'vue-router'
 export default {
     beforeCreate() {
      console.log("This is cokis function in about me " +this.$cookie.get('Userdetails'));
+
+     
+     var _id = this.$cookie.get('Userdetails')
+      console.log("Tjis is front end " + _id);
+      
+        axios
+        .get(`http://localhost:9000/users/me?id=${_id}`, )
+        .then(response => {
+
+      console.log(response.data);
+      })
+        .catch(function(error) {
+          console.log(error);
+        });
+  
+
   },
 components: {
      "app-about" : Navbar,
